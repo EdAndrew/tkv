9dcf7e9dbfaac1158827779056986f051d2027a5
Qiujiahao <qiujia145@163.com>
2017-06-20 22:29:54 +0800

    client in loop

--- client/tkv-client.go
+++ client/tkv-client.go
@@ -6,6 +6,7 @@ import "io"
 import "fmt"
 import "bytes"
 import "flag"
+import "strings"
 
 var serverIp *string = flag.String("h", "localhost", "Ip address of tkv server")
 var serverPort *string = flag.String("p", "6433", "Port of tkv server")
@@ -21,31 +22,37 @@ func main() {
 	}
 
 	var input string 
-	fmt.Scanf("%q", &input)
-	input += "\n"
+	for {
+		//fmt.Scanf("%q", &input)
+		input = "get 1"
+		if strings.Compare(input, "quit") == 0 {
+			break
+		}
+		input += "\n"
 
-	_, err = conn.Write([]byte(input))
-	if err != nil {
-		println(err.Error())
-		os.Exit(-1)
-	}
+		_, err = conn.Write([]byte(input))
+		if err != nil {
+			println(err.Error())
+			os.Exit(-1)
+		}
 
-	result, err := readFully(conn)
-	if err != nil {
-		println(err.Error())
-		os.Exit(-1)
+		result, err := readFully(conn)
+		if err != nil {
+			println(err.Error())
+			os.Exit(-1)
+		}
+		fmt.Println(string(result))
 	}
-	fmt.Println(string(result))
 	
 	os.Exit(0)
 }
 
 func readFully(conn net.Conn)([]byte, error) {
-	defer conn.Close()
 	var buf [512]byte
 	var cln []byte
 	cln = []byte("\n")
 	result := bytes.NewBuffer(nil)
+	result.Reset()
 
 	for {
 		n, err := conn.Read(buf[0:])	
